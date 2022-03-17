2b645859bc35e9499149b144b1675a13e0cac209
investmology.com <90382112+investmologycom@users.noreply.github.com>
2022-03-17 09:02:23 +0700

    Add files via upload

---
+++ serviceworkerku.js
@@ -0,0 +1,10 @@
+//<![CDATA[
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/sw.js').then(registration => {
+      }).catch(registrationError => {
+        console.log('SW registration failed: ', registrationError);
+      });
+    });
+  }
+//]]>
