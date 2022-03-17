6946f2e2fcebf7315110dc8b9615bd878b4b2871
investmology.com <90382112+investmologycom@users.noreply.github.com>
2022-03-17 14:20:20 +0700

    Delete localstorageku.js

--- localstorageku.js
+++
@@ -1,6 +0,0 @@
-window.onstorage = event => { // can also use window.addEventListener('storage', event => {
-  if (event.key != 'now') return;
-  alert(event.key + ':' + event.newValue + " at " + event.url);
-};
-
-localStorage.setItem('now', Date.now());
